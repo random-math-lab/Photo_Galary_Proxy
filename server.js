94e278b2b9853ad90dd8454448b4e7d6bd77097e
Steven Yoh <syoh@Stevens-MacBook-Pro-2.local>
2019-09-04 16:36:24 -0700

    Integrated all 3 services.

--- server.js
+++ server.js
@@ -5,34 +5,15 @@ const path = require('path');
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const axios = require('axios');
 
 app.use(morgan('dev'))
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static( './client/' ));
-
-//respond with the html file.
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public/lib/index.html'))
-// });
 
 app.get('/', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, './') + 'client/index.html');
 });
-
-// app.get('/api/photo/:id', (req, res) => {
-//   axios.get(`http://localhost:3002/api/photo/:id`)
-//   .then((data) => {
-//     res.status(200).send(data.data)
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//     res.status(500).send(err);
-//   });
-// });
-
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
